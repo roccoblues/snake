f2073701adfc7758c8b6f49e176caceca1ac68b2
Dennis Schön <mail@dennis-schoen.de>
2022-09-27 08:43:43 +0200

    wip

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use int_enum::{IntEnum, IntEnumError};
+use int_enum::IntEnum;
 use rand::prelude::*;
 use std::collections::VecDeque;
 use std::fmt;
@@ -36,20 +36,18 @@ impl Direction {
 #[derive(Debug, PartialEq)]
 pub enum Error {
     SnakeCrash,
-    InvalidDirection,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::SnakeCrash => f.write_str("The Snake crashed."),
-            Error::InvalidDirection => f.write_str("Invalid direction."),
         }
     }
 }
 
 #[derive(Debug)]
-struct Point {
+pub struct Point {
     x: u16,
     y: u16,
 }
@@ -66,118 +64,120 @@ impl Point {
     }
 }
 
-type Map = Vec<Vec<Tile>>;
-
-pub struct Game {
-    pub map: Map,
-    pub steps: u16,
+pub struct Snake {
+    body: VecDeque<Point>,
     direction: Direction,
-    snake: VecDeque<Point>,
 }
 
-impl Game {
-    pub fn new(size: u16) -> Game {
-        let mut map = new_map(size);
-        spawn_food(&mut map);
-        spawn_obstacles(&mut map);
-
-        let snake = spawn_snake(&mut map);
-
-        Game {
-            map,
-            snake,
-            direction: random_direction().unwrap(),
-            steps: 0,
+impl Snake {
+    pub fn set_direction(&mut self, direction: Direction) {
+        if self.direction.opposite() != direction {
+            return self.direction = direction;
         }
     }
+}
 
-    pub fn step(&mut self) -> Result<(), Error> {
-        // tile in front of the snake
-        let next = self.snake.front().unwrap().next(self.direction);
+type Tiles = Vec<Vec<Tile>>;
 
-        match self.map[next.x as usize][next.y as usize] {
-            Tile::Obstacle | Tile::Snake => {
-                set_tile(&mut self.map, &next, Tile::Crash);
-                return Err(Error::SnakeCrash);
+pub struct Map {
+    pub tiles: Tiles,
+    rng: ThreadRng,
+}
+
+impl Map {
+    pub fn new(size: u16) -> Map {
+        let mut tiles = vec![vec![Tile::Free; size.into()]; size.into()];
+        for x in 0..=size - 1 {
+            for y in 0..=size - 1 {
+                let tile = if x == 0 || y == 0 || x == size - 1 || y == size - 1 {
+                    Tile::Obstacle
+                } else {
+                    Tile::Free
+                };
+                tiles[x as usize][y as usize] = tile;
             }
-            Tile::Food => spawn_food(&mut self.map),
-            Tile::Free => {
-                // remove last snake tile to "move" the snake
-                let tail = self.snake.pop_back().unwrap();
-                set_tile(&mut self.map, &tail, Tile::Free);
+        }
+
+        let mut map = Map {
+            tiles,
+            rng: thread_rng(),
+        };
+
+        map.spawn_food();
+        map.spawn_obstacles();
+        map
+    }
+
+    fn tile(&self, point: &Point) -> Tile {
+        self.tiles[point.x as usize][point.y as usize]
+    }
+
+    fn set_tile(&mut self, point: &Point, tile: Tile) {
+        self.tiles[point.x as usize][point.y as usize] = tile;
+    }
+
+    fn random_empty_point(&mut self, distance: u16) -> Point {
+        let size = self.tiles.len() as u16;
+        loop {
+            let x = self.rng.gen_range(distance + 1..size - distance);
+            let y = self.rng.gen_range(distance + 1..size - distance);
+            let point = Point { x, y };
+            if self.tile(&point) == Tile::Free {
+                break point;
             }
-            Tile::Crash => unreachable!(),
-        }
-
-        // grow snake
-        set_tile(&mut self.map, &next, Tile::Snake);
-        self.snake.push_front(next);
-
-        self.steps += 1;
-
-        Ok(())
-    }
-
-    pub fn change_direction(&mut self, direction: Direction) -> Result<(), Error> {
-        if self.direction.opposite() == direction {
-            return Err(Error::InvalidDirection);
-        }
-        self.direction = direction;
-        Ok(())
-    }
-}
-
-fn random_direction() -> Result<Direction, IntEnumError<Direction>> {
-    Direction::from_int(thread_rng().gen_range(0..=3) as u8)
-}
-
-fn random_empty_point(map: &Map, distance: u16) -> Point {
-    let size = map.len() as u16;
-    loop {
-        let x = thread_rng().gen_range(distance + 1..size - distance);
-        let y = thread_rng().gen_range(distance + 1..size - distance);
-        if map[x as usize][y as usize] == Tile::Free {
-            break Point { x, y };
         }
     }
-}
 
-fn set_tile(map: &mut Map, point: &Point, tile: Tile) {
-    map[point.x as usize][point.y as usize] = tile;
-}
+    fn spawn_food(&mut self) {
+        let point = self.random_empty_point(1);
+        self.set_tile(&point, Tile::Food);
+    }
 
-fn new_map(size: u16) -> Map {
-    let mut tiles = vec![vec![Tile::Free; size.into()]; size.into()];
-    for x in 0..=size - 1 {
-        for y in 0..=size - 1 {
-            let tile = if x == 0 || y == 0 || x == size - 1 || y == size - 1 {
-                Tile::Obstacle
-            } else {
-                Tile::Free
-            };
-            tiles[x as usize][y as usize] = tile;
+    fn spawn_obstacles(&mut self) {
+        let size = self.tiles.len() as u16;
+        for _ in 0..=size / 3 {
+            let point = self.random_empty_point(0);
+            self.set_tile(&point, Tile::Obstacle);
         }
     }
-    tiles
-}
 
-fn spawn_food(map: &mut Map) {
-    let point = random_empty_point(&map, 1);
-    set_tile(map, &point, Tile::Food);
-}
-
-fn spawn_obstacles(map: &mut Map) {
-    let size = map.len() as u16;
-    for _ in 0..=size / 3 {
-        let point = random_empty_point(&map, 0);
-        set_tile(map, &point, Tile::Obstacle);
+    pub fn spawn_snake(&mut self) -> Snake {
+        let point = self.random_empty_point(3);
+        self.set_tile(&point, Tile::Snake);
+        let mut snake = Snake {
+            body: VecDeque::new(),
+            direction: random_direction(),
+        };
+        snake.body.push_front(point);
+        snake
     }
 }
 
-fn spawn_snake(map: &mut Map) -> VecDeque<Point> {
-    let mut snake = VecDeque::new();
-    let point = random_empty_point(&map, 3);
-    set_tile(map, &point, Tile::Snake);
-    snake.push_front(point);
-    snake
+pub fn step(map: &mut Map, snake: &mut Snake) -> Result<(), Error> {
+    // tile in front of the snake
+    let next = snake.body.front().unwrap().next(snake.direction);
+
+    match map.tile(&next) {
+        Tile::Obstacle | Tile::Snake => {
+            map.set_tile(&next, Tile::Crash);
+            return Err(Error::SnakeCrash);
+        }
+        Tile::Food => map.spawn_food(),
+        Tile::Free => {
+            // remove last snake tile to "move" the snake
+            let tail = snake.body.pop_back().unwrap();
+            map.set_tile(&tail, Tile::Free);
+        }
+        Tile::Crash => unreachable!(),
+    }
+
+    // grow snake
+    map.set_tile(&next, Tile::Snake);
+    snake.body.push_front(next);
+
+    Ok(())
+}
+
+pub fn random_direction() -> Direction {
+    Direction::from_int(thread_rng().gen_range(0..=3) as u8).unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crossterm::event::{poll, read, Event, KeyCode};
-use game::{Direction, Game};
+use game::{Direction, Map};
 use std::error::Error;
 use std::time::Duration;
 
@@ -11,33 +11,34 @@ const SIZE: u16 = 25;
 fn main() -> Result<(), Box<dyn Error>> {
     ui::init()?;
 
-    let mut game = Game::new(SIZE);
-    ui::draw_map(&game.map)?;
-    // ui::draw_score(game.steps());
+    let mut map = Map::new(SIZE);
 
+    let mut snake = map.spawn_snake();
     let mut crash = false;
+
+    ui::draw_map(&map.tiles)?;
 
     loop {
         if !crash {
-            match game.step() {
+            match game::step(&mut map, &mut snake) {
                 Err(game::Error::SnakeCrash) => crash = true,
                 _ => {}
             }
-            ui::draw_map(&game.map)?
+            ui::draw_map(&map.tiles)?
         }
 
-        if poll(Duration::from_millis(100))? {
+        if poll(Duration::from_millis(150))? {
             let event = read().unwrap();
             if event == Event::Key(KeyCode::Esc.into()) {
                 break;
             } else if event == Event::Key(KeyCode::Up.into()) {
-                game.change_direction(Direction::Up).ok();
+                snake.set_direction(Direction::Up);
             } else if event == Event::Key(KeyCode::Down.into()) {
-                game.change_direction(Direction::Down).ok();
+                snake.set_direction(Direction::Down);
             } else if event == Event::Key(KeyCode::Left.into()) {
-                game.change_direction(Direction::Left).ok();
+                snake.set_direction(Direction::Left);
             } else if event == Event::Key(KeyCode::Right.into()) {
-                game.change_direction(Direction::Right).ok();
+                snake.set_direction(Direction::Right);
             };
         }
     }
