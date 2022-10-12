dd7189de13e32f9c5b6e2c6a17e4b8e30a8bddf2
Dennis Schön <mail@dennis-schoen.de>
2022-10-12 10:48:38 +0200

    use point in open list

--- src/path.rs
+++ src/path.rs
@@ -1,15 +1,15 @@
 use crate::game::{Cell, Direction, Grid};
-use log::{debug, error, info, log_enabled, Level};
+use log::debug;
 use rand::prelude::*;
 use std::collections::HashMap;
 
-#[derive(Debug, Clone, PartialEq, Hash, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Hash, Eq)]
 struct Point {
     x: usize,
     y: usize,
 }
 
-#[derive(Default, Clone, Debug)]
+#[derive(Default, Debug)]
 struct CellInfo {
     parent: Option<Point>,
     g: usize, // the movement cost to move from the starting point to a given square on the grid, following the path generated to get there.
@@ -22,27 +22,29 @@ type CellDetails = HashMap<Point, CellInfo>;
 // Calculates path as a vector of directions using the A* Search Algorithm.
 // https://www.geeksforgeeks.org/a-search-algorithm/
 pub fn solve(grid: &Grid, (start_x, start_y): (usize, usize)) -> Vec<Direction> {
-    let (target_x, target_y) = find_target(grid);
+    let start = Point {
+        x: start_x,
+        y: start_y,
+    };
+
+    let target = find_target(grid);
 
     // Use a hashmap to hold the details of a cell.
     let mut cell_details = HashMap::new();
 
     // Create an open list.
-    let mut open_list: Vec<(usize, usize)> = Vec::with_capacity(grid.len() * grid.len());
+    let mut open_list: Vec<Point> = Vec::with_capacity(grid.len() * grid.len());
 
     // Create a closed list and initialise it to false which means that no cell
     // has been included yet. This closed list is implemented as a boolean 2D array.
     let mut closed_list = vec![vec![false; grid.len()]; grid.len()];
 
     // put the starting node on the open list
-    open_list.push((start_x, start_y));
+    open_list.push(start);
 
     // and record it details
     cell_details.insert(
-        Point {
-            x: start_x,
-            y: start_y,
-        },
+        start,
         CellInfo {
             ..Default::default()
         },
@@ -53,44 +55,38 @@ pub fn solve(grid: &Grid, (start_x, start_y): (usize, usize)) -> Vec<Direction> 
 
         // pop the node with the lowest f on the open list
         let i = lowest_f(&open_list, &cell_details);
-        let (x, y) = open_list.swap_remove(i);
+        let p = open_list.swap_remove(i);
 
         // push it on the closed list
-        closed_list[x][y] = true;
+        closed_list[p.x][p.y] = true;
 
-        let successors = generate_successors(x, y, grid);
-        for (next_x, next_y) in successors.into_iter() {
+        let successors = generate_successors(&p, grid);
+        for next in successors.into_iter() {
             // if the successor is already on the closed list we ignore it
-            if closed_list[next_x][next_y] {
+            if closed_list[next.x][next.y] {
                 continue;
             }
 
             // if successor is the target, stop search
-            if next_x == target_x && next_y == target_y {
+            if next.x == target.x && next.y == target.y {
                 cell_details.insert(
-                    Point {
-                        x: next_x,
-                        y: next_y,
-                    },
+                    next,
                     CellInfo {
-                        parent: Some(Point { x, y }),
+                        parent: Some(p),
                         ..Default::default()
                     },
                 );
-                return generate_path(next_x, next_y, &cell_details);
+                return generate_path(&next, &cell_details);
             }
 
             // compute g,h and f for successor
-            let info = cell_details.get(&Point { x, y }).unwrap();
+            let info = cell_details.get(&p).unwrap();
             let g = info.g + 1;
-            let h = manhatten_distance(next_x, next_y, target_x, target_y);
+            let h = manhatten_distance(next, target);
             let f = g + h as usize;
 
             // if a node with the same position as successor is in the open list
-            match cell_details.get_mut(&Point {
-                x: next_x,
-                y: next_y,
-            }) {
+            match cell_details.get_mut(&next) {
                 Some(next_info) => {
                     // which has a lower f than successor, skip this successor
                     if next_info.f < f {
@@ -100,22 +96,19 @@ pub fn solve(grid: &Grid, (start_x, start_y): (usize, usize)) -> Vec<Direction> 
                     next_info.f = f;
                     next_info.g = g;
                     next_info.h = h;
-                    next_info.parent = Some(Point { x, y });
+                    next_info.parent = Some(p);
                 }
                 None => {
                     // otherwise, add the node to the open list
-                    open_list.push((next_x, next_y));
+                    open_list.push(next);
                     // record the details of this cell
                     cell_details.insert(
-                        Point {
-                            x: next_x,
-                            y: next_y,
-                        },
+                        next,
                         CellInfo {
                             g,
                             h,
                             f,
-                            parent: Some(Point { x, y }),
+                            parent: Some(p),
                         },
                     );
                 }
@@ -125,35 +118,34 @@ pub fn solve(grid: &Grid, (start_x, start_y): (usize, usize)) -> Vec<Direction> 
 
     // We didn't find a clear path.
     // If we have clear successors we pick a random one.
-    let successors = generate_successors(start_x, start_y, grid);
+    let successors = generate_successors(&start, grid);
     if successors.len() > 0 {
-        let (next_x, next_y) =
-            successors[thread_rng().gen_range(0..=successors.len() - 1) as usize];
-        return vec![get_direction(start_x, start_y, next_x, next_y)];
+        let next = successors[thread_rng().gen_range(0..=successors.len() - 1) as usize];
+        return vec![get_direction(&start, &next)];
     }
 
     // Brace for impact!
     vec![]
 }
 
-fn find_target(grid: &Grid) -> (usize, usize) {
+fn find_target(grid: &Grid) -> Point {
     for (x, row) in grid.iter().enumerate() {
         for (y, cell) in row.iter().enumerate() {
             if *cell == Cell::Food {
-                return (x, y);
+                return Point { x, y };
             }
         }
     }
     unreachable!("No food found in grid!")
 }
 
-fn lowest_f(list: &[(usize, usize)], cell_details: &CellDetails) -> usize {
+fn lowest_f(list: &[Point], cell_details: &CellDetails) -> usize {
     assert!(list.len() > 0);
 
     let mut f = usize::MAX;
     let mut i = 0;
-    for (n, (x, y)) in list.iter().enumerate() {
-        match cell_details.get(&Point { x: *x, y: *y }) {
+    for (n, p) in list.iter().enumerate() {
+        match cell_details.get(&p) {
             Some(info) => {
                 if info.f < f {
                     f = info.f;
@@ -178,44 +170,42 @@ fn lowest_f(list: &[(usize, usize)], cell_details: &CellDetails) -> usize {
 // S --> South  (x+1, y  )
 // E --> East   (x,   y+1)
 // W --> West   (x,   y-1)
-fn generate_successors(x: usize, y: usize, grid: &Grid) -> Vec<(usize, usize)> {
-    let mut result: Vec<(usize, usize)> = Vec::with_capacity(4);
+fn generate_successors(p: &Point, grid: &Grid) -> Vec<Point> {
+    let mut result: Vec<Point> = Vec::with_capacity(4);
 
     // north
-    if x > 0 {
-        result.push((x - 1, y))
+    if p.x > 0 {
+        result.push(Point { x: p.x - 1, ..*p });
     }
     // south
-    if x + 1 < grid.len() {
-        result.push((x + 1, y))
+    if p.x + 1 < grid.len() {
+        result.push(Point { x: p.x + 1, ..*p });
     }
     // east
-    if y + 1 < grid.len() {
-        result.push((x, y + 1))
+    if p.y + 1 < grid.len() {
+        result.push(Point { y: p.y + 1, ..*p });
     }
     // west
-    if y > 0 {
-        result.push((x, y - 1))
+    if p.y > 0 {
+        result.push(Point { y: p.y - 1, ..*p })
     }
 
     result
         .into_iter()
-        .filter(|(x, y)| grid[*x][*y] == Cell::Free || grid[*x][*y] == Cell::Food)
+        .filter(|p| grid[p.x][p.y] == Cell::Free || grid[p.x][p.y] == Cell::Food)
         .collect()
 }
 
-fn generate_path(start_x: usize, start_y: usize, cell_details: &CellDetails) -> Vec<Direction> {
+fn generate_path(start: &Point, cell_details: &CellDetails) -> Vec<Direction> {
     let mut directions: Vec<Direction> = Vec::new();
-    let mut x = start_x;
-    let mut y = start_y;
+    let mut p = start;
     loop {
-        match cell_details.get(&Point { x, y }) {
+        match cell_details.get(p) {
             Some(info) => match &info.parent {
                 Some(parent) => {
-                    let direction = get_direction(parent.x, parent.y, x, y);
+                    let direction = get_direction(parent, p);
                     directions.push(direction);
-                    x = parent.x;
-                    y = parent.y;
+                    p = parent;
                 }
                 None => break,
             },
@@ -225,41 +215,27 @@ fn generate_path(start_x: usize, start_y: usize, cell_details: &CellDetails) -> 
     directions
 }
 
-fn get_direction(from_x: usize, from_y: usize, to_x: usize, to_y: usize) -> Direction {
-    if to_x > from_x {
+fn get_direction(from: &Point, to: &Point) -> Direction {
+    if to.x > from.x {
         Direction::East
-    } else if to_x < from_x {
+    } else if to.x < from.x {
         Direction::West
-    } else if to_y > from_y {
+    } else if to.y > from.y {
         Direction::South
     } else {
         Direction::North
     }
 }
 
-fn manhatten_distance(x: usize, y: usize, target_x: usize, target_y: usize) -> usize {
-    let distance = i32::abs(x as i32 - target_x as i32) + i32::abs(y as i32 - target_y as i32);
+fn manhatten_distance(start: Point, target: Point) -> usize {
+    let distance =
+        i32::abs(start.x as i32 - target.x as i32) + i32::abs(start.y as i32 - target.y as i32);
     distance.try_into().unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn manhatten_distance_equal() {
-        assert_eq!(manhatten_distance(1, 1, 1, 1), 0);
-    }
-
-    #[test]
-    fn manhatten_distance_positive() {
-        assert_eq!(manhatten_distance(1, 1, 3, 4), 5);
-    }
-
-    #[test]
-    fn manhatten_distance_negative() {
-        assert_eq!(manhatten_distance(5, 5, 1, 3), 6);
-    }
 
     #[test]
     fn solve_path_simple() {
